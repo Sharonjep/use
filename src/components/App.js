47a9814cf430422b1bdac0b63de0d8ae8893e32c
Sharonjep <jepngetichs65@gmail.com>
2023-01-31 23:13:38 -0800

    useState

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+
+function App(){
+    const [state,setState] = useState({count: 7, theme: 'blue'})
+    const count =state.count
+    const theme =state.theme
+
+
+    function decrementCount(){
+        setState (prevState=>{
+            return {...prevState, count:prevState.count -1}
+
+        })
+    }
+    function incrementCount(){
+        setState (prevState=>{
+            return {...prevState, count:prevState.count +1}
+        })
+
+    }
+
+
+    return (
+        <>
+         <button  onClick={decrementCount}>-</button>
+         <span>{count}</span>
+         <span>{theme}</span>
+
+         <button onClick={incrementCount}>+</button>
+
+
+        </>
+    )
+    }
+export default App
